8afe90751e8a7eeb19a41b8bb230f85fd79d6c41
Roman-code-web <selenerg.275@gmail.com>
2023-01-05 13:48:39 -0500

    componente informacion terminado

--- src/app/componentes/componentes.module.ts
+++ src/app/componentes/componentes.module.ts
@@ -1,17 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BannerComponent } from './banner/banner.component';
+import { InformacionComponent } from './informacion/informacion.component';
 
 
 
 @NgModule({
   declarations: [
-    BannerComponent
+    BannerComponent,
+    InformacionComponent
   ],
   imports: [
     CommonModule
   ],exports:[
-    BannerComponent
+    BannerComponent,
+    InformacionComponent
   ]
 })
 export class ComponentesModule { }
