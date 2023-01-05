88caaf66d9a03db68f174196fe0cd13a13b7be3f
Roman-code-web <selenerg.275@gmail.com>
2023-01-05 13:00:07 -0500

    componente banner y animacion

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { ContactanosComponent } from './contactanos/contactanos.component';
+import { ComponentesModule } from '../componentes/componentes.module';
 
 
 
@@ -11,7 +12,8 @@ import { ContactanosComponent } from './contactanos/contactanos.component';
     ContactanosComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    ComponentesModule
   ],exports:[
     HomeComponent,
     ContactanosComponent
