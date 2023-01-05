feea8e6d212f06fda8eaa0d8ebb511d2b4370282
Roman-code-web <selenerg.275@gmail.com>
2023-01-05 12:29:25 -0500

    rutas y header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactanosComponent } from './pages/contactanos/contactanos.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"" , component:HomeComponent},
+  {path:"contactanos", component:ContactanosComponent},
+  {path:"**" , pathMatch:"full", redirectTo:""}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home/home.component';
+import { ContactanosComponent } from './contactanos/contactanos.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    ContactanosComponent
+  ],
   imports: [
     CommonModule
+  ],exports:[
+    HomeComponent,
+    ContactanosComponent
   ]
 })
 export class PagesModule { }
