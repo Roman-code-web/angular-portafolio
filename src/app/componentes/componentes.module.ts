af22b3a5890157b498d2b4c6b3310f9d988d51a4
Roman-code-web <selenerg.275@gmail.com>
2023-01-05 13:50:57 -0500

    envio solicitado por el profesor

--- src/app/componentes/componentes.module.ts
+++ src/app/componentes/componentes.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BannerComponent } from './banner/banner.component';
 import { InformacionComponent } from './informacion/informacion.component';
+import { SkilsComponent } from './skils/skils.component';
 
 
 
 @NgModule({
   declarations: [
     BannerComponent,
-    InformacionComponent
+    InformacionComponent,
+    SkilsComponent
   ],
   imports: [
     CommonModule
