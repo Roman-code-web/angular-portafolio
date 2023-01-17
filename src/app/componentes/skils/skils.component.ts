c3b94744b5baf83e1430fb7932d32ac30be9903d
Roman-code-web <selenerg.275@gmail.com>
2023-01-16 21:32:06 -0500

    modificacion de seccion skils

--- src/app/componentes/skils/skils.component.ts
+++ src/app/componentes/skils/skils.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class SkilsComponent {
   listaSkills=[{
+    "imagen":"https://img.icons8.com/fluency/512/html-5.png",
     "nombre":"HTML",
     "nivel":"Avanzado",
     "porcentaje":"100%",
@@ -15,59 +16,65 @@ export class SkilsComponent {
     "color":"bg-primary"
   },
   {
+    "imagen":"https://img.icons8.com/color/512/css3.png",
     "nombre":"CSS",
     "nivel":"Avanzado",
     "porcentaje":"90%",
     "value":"90",
     "style":"width: 90%",
-    "color":"bg-success"
+  
   },
   {
+    "imagen":"https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
     "nombre":"BOOTSTRAP",
     "nivel":"Avanzado",
     "porcentaje":"80%",
     "value":"80",
     "style":"width: 80%",
-    "color":"bg-secondary"
+
   },
   {
+    "imagen":"https://img.icons8.com/fluency/512/javascript.png",
     "nombre":"JS",
     "nivel":"Intermedio",
     "porcentaje":"70%",
     "value":"70",
     "style":"width: 70%",
-    "color":"bg-warning"
+
   },
   {
+    "imagen":"https://img.icons8.com/officel/512/php-logo.png",
     "nombre":"PHP",
     "nivel":"Intermedio",
     "porcentaje":"50%",
     "value":"50",
     "style":"width: 50%",
-    "color":"bg-info"
   },
   {
+    "imagen":"https://img.icons8.com/color/512/mongodb.png",
     "nombre":"MONGODB",
     "nivel":"Intermedio",
     "porcentaje":"70%",
     "value":"70",
     "style":"width: 70%",
-    "color":"bg-morado"
-  },{
+  
+  },
+  {
+    "imagen":"https://img.icons8.com/color/512/microsoft-sql-server.png",
     "nombre":"SQL SERVER",
     "nivel":"Intermedio",
     "porcentaje":"70%",
     "value":"70",
     "style":"width: 70%",
-    "color":"bg-orange"
+
   }
   ,{
+    "imagen":"https://img.icons8.com/color/512/mysql-logo.png",
     "nombre":"MYSQL",
     "nivel":"Intermedio",
     "porcentaje":"70%",
     "value":"70",
     "style":"width: 70%",
-    "color":"bg-rosado"
   }
 ];
 }
